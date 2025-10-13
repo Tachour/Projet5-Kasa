e13949b36da2cd88f96e2815036068c51ac5571c
Tarek Achour <tarek-achour@outllok.fr>
2025-10-13 16:43:36 +0200

    correction animation du  collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,28 +10,28 @@ export default function About() {
 
       <div className="about__collapses">
         <Collapse title="Fiabilité">
-          <p>
+          <p className="collapse__text">
             Les annonces sont vérifiées et régulièrement mises à jour pour vous
             garantir des informations fiables et à jour.
           </p>
         </Collapse>
 
         <Collapse title="Respect">
-          <p>
+          <p className="collapse__text">
             La courtoisie et le respect guident nos échanges : hôtes et
             voyageurs s’engagent à préserver la qualité des logements.
           </p>
         </Collapse>
 
         <Collapse title="Service">
-          <p>
+          <p className="collapse__text">
             Un support réactif et des outils simples vous accompagnent avant,
             pendant et après votre séjour.
           </p>
         </Collapse>
 
         <Collapse title="Sécurité">
-          <p>
+          <p className="collapse__text">
             Les paiements sont sécurisés et vos données personnelles protégées
             selon les meilleures pratiques du secteur.
           </p>
